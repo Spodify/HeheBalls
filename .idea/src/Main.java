8f13687a054075cdde0674eed253cd7334078822
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-01 21:00:14 +0200

    main class and folders

---
+++ .idea/src/Main.java
@@ -0,0 +1,14 @@
+import ea.Game;
+import ea.Scene;
+import ea.Vector;
+import ea.event.MouseButton;
+import ea.event.MouseClickListener;
+
+public class Main {
+    public static void main(String[] args) {
+        game sUs = new game(1280, 720);
+        sUs.alexaDisableGravity();
+        //Lvl1 l = new Lvl1();
+
+    }
+}
