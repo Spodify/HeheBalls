c5ef4381388e34a342cadf2d77c2b047e68cae43
Spodify <felixle787@gmail.com>
2022-06-05 15:29:45 +0200

    png

--- src/src/flaggen.java
+++ src/src/flaggen.java
@@ -8,14 +8,14 @@ public class flaggen extends Figur{
 
     public flaggen(){
         super("normal","rsc/Flaggen Leicht GIF/England.png");
-        String [] flaggenleicht = {"rsc/Flaggen Leicht GIF/Belgien.gif", "rsc/Flaggen Leicht GIF/Brasilien.gif", "rsc/Flaggen Leicht GIF/China.gif", "rsc/Flaggen Leicht GIF/Deutschland.gif","rsc/Flaggen Leicht GIF/England.gif", "rsc/Flaggen Leicht GIF/Finnland.gif",
-                                    "rsc/Flaggen Leicht GIF/Frankreich.gif", "rsc/Flaggen Leicht GIF/Israel.gif", "rsc/Flaggen Leicht GIF/Italien.gif", "rsc/Flaggen Leicht GIF/Japan.gif", "rsc/Flaggen Leicht GIF/Kanada.gif", "rsc/Flaggen Leicht GIF/Kroatien.gif",
-                                    "rsc/Flaggen Leicht GIF/Mexiko.gif", "rsc/Flaggen Leicht GIF/Niederlande.gif", "rsc/Flaggen Leicht GIF/Norwegen.gif", "rsc/Flaggen Leicht GIF/Polen.gif", "rsc/Flaggen Leicht GIF/Portugal.gif", "rsc/Flaggen Leicht GIF/Russland.gif",
-                                    "rsc/Flaggen Leicht GIF/Schweden.gif", "rsc/Flaggen Leicht GIF/Schweiz.gif", "rsc/Flaggen Leicht GIF/Spanien.gif", "rsc/Flaggen Leicht GIF/Suedkorea.gif","rsc/Flaggen Leicht GIF/Ukraine.gif"};
+        String [] flaggenleicht = {"rsc/Flaggen Leicht PNG/Belgien.png", "rsc/Flaggen Leicht PNG/Brasilien.png", "rsc/Flaggen Leicht PNG/China.png", "rsc/Flaggen Leicht PNG/Deutschland.png","rsc/Flaggen Leicht PNG/England.png", "rsc/Flaggen Leicht PNG/Finnland.png",
+                                    "rsc/Flaggen Leicht PNG/Frankreich.png", "rsc/Flaggen Leicht PNG/Israel.png", "rsc/Flaggen Leicht PNG/Italien.png", "rsc/Flaggen Leicht PNG/Japan.png", "rsc/Flaggen Leicht PNG/Kanada.png", "rsc/Flaggen Leicht PNG/Kroatien.png",
+                                    "rsc/Flaggen Leicht PNG/Mexiko.png", "rsc/Flaggen Leicht PNG/Niederlande.png", "rsc/Flaggen Leicht PNG/Norwegen.png", "rsc/Flaggen Leicht PNG/Polen.png", "rsc/Flaggen Leicht PNG/Portugal.png", "rsc/Flaggen Leicht PNG/Russland.png",
+                                    "rsc/Flaggen Leicht PNG/Schweden.png", "rsc/Flaggen Leicht PNG/Schweiz.png", "rsc/Flaggen Leicht PNG/Spanien.png", "rsc/Flaggen Leicht PNG/Suedkorea.png","rsc/Flaggen Leicht PNG/Ukraine.png"};
 
-        String [] flaggenmittel = {"rsc/Flaggen Mittel GIF/Afghanistan.gif", "rsc/Flaggen Mittel GIF/Algerien.gif", "rsc/Flaggen Mittel GIF/Bangladesch.gif", "rsc/Flaggen Mittel GIF/Chile.gif", "rsc/Flaggen Mittel GIF/Estland.gif", "rsc/Flaggen Mittel GIF/Ghana.gif",
-                                    "rsc/Flaggen Mittel GIF/Irland.gif", "rsc/Flaggen Mittel GIF/Jamaika.gif", "rsc/Flaggen Mittel GIF/Kolumbien.gif", "rsc/Flaggen Mittel GIF/Kuba.gif", "rsc/Flaggen Mittel GIF/Malta.gif", "rsc/Flaggen Mittel GIF/Nepal.gif",
-                                    "rsc/Flaggen Mittel GIF/Niger.gif", "rsc/Flaggen Mittel GIF/Nordmazedonien.gif", "rsc/Flaggen Mittel GIF/Philippinen.gif", "rsc/Flaggen Mittel GIF/Saudi Arabien.gif", "rsc/Flaggen Mittel GIF/Senegal.gif", "rsc/Flaggen Mittel GIF/Serbien.gif",
-                                    "rsc/Flaggen Mittel GIF/Suedafrika.gif", "rsc/Flaggen Mittel GIF/Zypern.gif"};
+        String [] flaggenmittel = {"rsc/Flaggen Mittel PNG/Afghanistan.png", "rsc/Flaggen Mittel PNG/Algerien.png", "rsc/Flaggen Mittel PNG/Bangladesch.png", "rsc/Flaggen Mittel PNG/Chile.png", "rsc/Flaggen Mittel PNG/Estland.png", "rsc/Flaggen Mittel PNG/Ghana.png",
+                                    "rsc/Flaggen Mittel PNG/Irland.png", "rsc/Flaggen Mittel PNG/Jamaika.png", "rsc/Flaggen Mittel PNG/Kolumbien.png", "rsc/Flaggen Mittel PNG/Kuba.png", "rsc/Flaggen Mittel PNG/Malta.png", "rsc/Flaggen Mittel PNG/Nepal.png",
+                                    "rsc/Flaggen Mittel PNG/Niger.png", "rsc/Flaggen Mittel PNG/Nordmazedonien.png", "rsc/Flaggen Mittel PNG/Philippinen.png", "rsc/Flaggen Mittel PNG/Saudi Arabien.png", "rsc/Flaggen Mittel PNG/Senegal.png", "rsc/Flaggen Mittel PNG/Serbien.png",
+                                    "rsc/Flaggen Mittel PNG/Suedafrika.png", "rsc/Flaggen Mittel PNG/Zypern.png"};
     }
 }
