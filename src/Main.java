7432d99c42259395559f1b0dba7ab7900cd97683
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-07 20:34:33 +0200

    bild

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import src.flaggen;
 import java.awt.*;
 
 public class Main {
-
+    String Flagge;
     public static void main(String[] args) {
 
         flaggen f1 = new flaggen();
--- src/flaggen.java
+++ src/flaggen.java
@@ -1,13 +1,17 @@
 package src;
 
+
 import ea.edu.Figur;
 
+import java.util.Random;
 
-public class flaggen extends Figur{
 
-
+public class flaggen {
+        public Figur flagge;
+        String flaggenName;
+        int index;
     public flaggen(){
-        super("normal","rsc/Flaggen Leicht GIF/Deutschland.gif");
+
 
         String [] flaggenleicht = {"rsc/Flaggen Leicht GIF/Belgien.gif", "rsc/Flaggen Leicht GIF/Brasilien.gif", "rsc/Flaggen Leicht GIF/China.gif", "rsc/Flaggen Leicht GIF/Deutschland.gif","rsc/Flaggen Leicht GIF/England.gif", "rsc/Flaggen Leicht GIF/Finnland.gif",
                                     "rsc/Flaggen Leicht GIF/Frankreich.gif", "rsc/Flaggen Leicht GIF/Israel.gif", "rsc/Flaggen Leicht GIF/Italien.gif", "rsc/Flaggen Leicht GIF/Japan.gif", "rsc/Flaggen Leicht GIF/Kanada.gif", "rsc/Flaggen Leicht PNG/Kroatien.png",
@@ -18,5 +22,17 @@ public class flaggen extends Figur{
                                     "rsc/Flaggen Mittel PNG/Irland.png", "rsc/Flaggen Mittel PNG/Jamaika.png", "rsc/Flaggen Mittel PNG/Kolumbien.png", "rsc/Flaggen Mittel PNG/Kuba.png", "rsc/Flaggen Mittel PNG/Malta.png", "rsc/Flaggen Mittel PNG/Nepal.png",
                                     "rsc/Flaggen Mittel PNG/Niger.png", "rsc/Flaggen Mittel PNG/Nordmazedonien.png", "rsc/Flaggen Mittel PNG/Philippinen.png", "rsc/Flaggen Mittel PNG/Saudi Arabien.png", "rsc/Flaggen Mittel PNG/Senegal.png", "rsc/Flaggen Mittel PNG/Serbien.png",
                                     "rsc/Flaggen Mittel PNG/Suedafrika.png", "rsc/Flaggen Mittel PNG/Zypern.png"};
+        Random rand = new Random();
+        int index = rand.nextInt(flaggenleicht.length);
+        flaggenName = (flaggenleicht[index]);
+
+        //flaggenName = ("rsc/Flaggen Leicht GIF/Belgien.gif");
+
+
+
+
+        flagge = new Figur("normal",flaggenName);
+
+
     }
 }
