0a1087b594b976fb62ad2a84088f57de86f993a3
Spodify <felixle787@gmail.com>
2022-06-07 21:06:12 +0200

    mittel

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,12 +1,13 @@
 import ea.edu.Figur;
 
 public class TitleScreen {
-    public Figur flaggenLecht;
+    public Figur flaggenLeicht;
     public Figur flaggenMittel;
     TitleScreen(){
-        flaggenLecht = new Figur ("button","rsc/Flaggen Leicht GIF/Belgien.gif");
-        flaggenLecht.setzeMittelpunkt(0,-4.3);
-
+        flaggenLeicht = new Figur ("button","rsc/Flaggen Leicht GIF/Belgien.gif");
+        flaggenLeicht.setzeMittelpunkt(0,-4.3);
+        flaggenMittel = new Figur ("button","rsc/Flaggen Mittel GIF/Afghanistan.gif");
+        flaggenMittel.setzeMittelpunkt(0,-4.3);
     }
 
 
--- src/flaggen.java
+++ src/flaggen.java
@@ -18,10 +18,10 @@ public class flaggen {
                                     "rsc/Flaggen Leicht GIF/Mexiko.gif", "rsc/Flaggen Leicht GIF/Niederlande.gif", "rsc/Flaggen Leicht GIF/Norwegen.gif", "rsc/Flaggen Leicht GIF/Polen.gif", "rsc/Flaggen Leicht GIF/Portugal.gif", "rsc/Flaggen Leicht GIF/Russland.gif",
                                     "rsc/Flaggen Leicht GIF/Schweden.gif", "rsc/Flaggen Leicht GIF/Schweiz.gif", "rsc/Flaggen Leicht GIF/Spanien.gif", "rsc/Flaggen Leicht GIF/Suedkorea.gif","rsc/Flaggen Leicht GIF/Ukraine.gif"};
 
-        String [] flaggenmittel = {"rsc/Flaggen Mittel GIF/Afghanistan.gif", "rsc/Flaggen Mittel GIF/Algerien.gif", "rsc/Flaggen Mittel PNG/Bangladesch.png", "rsc/Flaggen Mittel PNG/Chile.png", "rsc/Flaggen Mittel PNG/Estland.png", "rsc/Flaggen Mittel PNG/Ghana.png",
-                                    "rsc/Flaggen Mittel PNG/Irland.png", "rsc/Flaggen Mittel PNG/Jamaika.png", "rsc/Flaggen Mittel PNG/Kolumbien.png", "rsc/Flaggen Mittel PNG/Kuba.png", "rsc/Flaggen Mittel PNG/Malta.png", "rsc/Flaggen Mittel PNG/Nepal.png",
-                                    "rsc/Flaggen Mittel PNG/Niger.png", "rsc/Flaggen Mittel PNG/Nordmazedonien.png", "rsc/Flaggen Mittel PNG/Philippinen.png", "rsc/Flaggen Mittel PNG/Saudi Arabien.png", "rsc/Flaggen Mittel PNG/Senegal.png", "rsc/Flaggen Mittel PNG/Serbien.png",
-                                    "rsc/Flaggen Mittel PNG/Suedafrika.png", "rsc/Flaggen Mittel PNG/Zypern.png"};
+        String [] flaggenmittel = {"rsc/Flaggen Mittel GIF/Afghanistan.gif", "rsc/Flaggen Mittel GIF/Algerien.gif", "rsc/Flaggen Mittel GIF/Bangladesch.gif", "rsc/Flaggen Mittel GIF/Chile.gif", "rsc/Flaggen Mittel GIF/Estland.gif", "rsc/Flaggen Mittel GIF/Ghana.gif",
+                                    "rsc/Flaggen Mittel GIF/Irland.gif", "rsc/Flaggen Mittel GIF/Jamaika.gif", "rsc/Flaggen Mittel GIF/Kolumbien.gif", "rsc/Flaggen Mittel GIF/Kuba.gif", "rsc/Flaggen Mittel GIF/Malta.gif", "rsc/Flaggen Mittel GIF/Nepal.gif",
+                                    "rsc/Flaggen Mittel GIF/Niger.gif", "rsc/Flaggen Mittel GIF/Nordmazedonien.gif", "rsc/Flaggen Mittel GIF/Philipinen.gif", "rsc/Flaggen Mittel GIF/Saudi Arabien.gif", "rsc/Flaggen Mittel GIF/Senegal.gif", "rsc/Flaggen Mittel GIF/Serbien.gif",
+                                    "rsc/Flaggen Mittel GIF/Suedafrika.gif", "rsc/Flaggen Mittel GIF/Zypern.gif"};
         Random rand = new Random();
         int index = rand.nextInt(flaggenleicht.length);
         flaggenName = (flaggenleicht[index]);
