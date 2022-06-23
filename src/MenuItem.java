adb2c2234a42184557e818000e4f8fb5694cf352
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-23 22:04:05 +0200

    cbt

---
+++ src/MenuItem.java
@@ -0,0 +1,16 @@
+import ea.edu.Text;
+
+import java.awt.*;
+
+public class MenuItem extends Rectangle {
+ private Text lable;
+    public MenuItem(String center, String lableText){
+
+        Text lable = new Text(lableText, 1);
+        lable.setzeSichtbar(true);
+        System.out.println("f");
+
+    }
+
+
+}
